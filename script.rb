0f4a4b3386140e69c79b77b9eb97fb87ea0844d3
Philippe Roy <renaissance542@gmail.com>
2021-06-15 14:57:53 -0400

    refactored to please Rubocop, added string interpolation for victory message

--- script.rb
+++ script.rb
@@ -4,20 +4,18 @@ require 'pry'
 
 # updates and returns board state
 class GameBoard
-  attr_reader :next_move, :game_over, :board, :movelist
+  attr_reader :next_move, :board, :movelist
 
   def initialize
     @movelist = []
-    @game_over = false
     @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     @next_move = 'X'
   end
 
   def move(int)
     return false if
-      @game_over ||
-      !int.between?(1, 9) ||
-      !@board[int-1].is_a?(Integer)
+      !int.between?(1, 9) || # invalid input
+      !@board[int-1].is_a?(Integer) # already marked X or O
 
     @movelist.push(int)
     @board[int - 1] = @next_move
@@ -33,21 +31,35 @@ class GameBoard
     @next_move = 'X'
   end
 
+  # board is full or game is won
+  def game_over
+    @movelist.length == 9 || check_win
+  end
+
   private
 
+  # checks board for 3 in a row win condition
   def check_win
-    @game_over = true if
-    #check 3 in a row
+    check_horizontal_win ||
+      check_verticle_win ||
+      check_diagonal_win
+  end
+
+  def check_horizontal_win
     (@board[0..2].all? @board[0]) ||
-    (@board[3..5].all? @board[3]) ||
-    (@board[6..8].all? @board[6]) ||
-    #check 3 in columns
+      (@board[3..5].all? @board[3]) ||
+      (@board[6..8].all? @board[6])
+  end
+
+  def check_verticle_win
     ([@board[0], @board[3], @board[6]].all? @board[0]) ||
-    ([@board[1], @board[4], @board[7]].all? @board[1]) ||
-    ([@board[2], @board[5], @board[8]].all? @board[2]) ||
-    #check diagonals
+      ([@board[1], @board[4], @board[7]].all? @board[1]) ||
+      ([@board[2], @board[5], @board[8]].all? @board[2])
+  end
+
+  def check_diagonal_win
     ([@board[0], @board[4], @board[8]].all? @board[0]) ||
-    ([@board[6], @board[4], @board[2]].all? @board[6]) 
+      ([@board[6], @board[4], @board[2]].all? @board[6])
   end
 end
 
@@ -66,7 +78,9 @@ class Game
       puts 'Invalid move.\n' unless @board.move(move)
     end
 
-    puts 'Game over! Somebody Won!'
+    print_board
+    winner = @board.movelist.length.even? ? @player2 : @player1
+    puts "Game over! #{winner} Won!"
   end
 
   private
@@ -79,10 +93,12 @@ class Game
   end
 
   def print_board
-    puts ''
-    puts "#{@board.board[0]} #{@board.board[1]} #{@board.board[2]}"
-    puts "#{@board.board[3]} #{@board.board[4]} #{@board.board[5]}"
-    puts "#{@board.board[6]} #{@board.board[7]} #{@board.board[8]}"
+    puts <<~BOARD
+    
+      #{@board.board[0]} #{@board.board[1]} #{@board.board[2]}
+      #{@board.board[3]} #{@board.board[4]} #{@board.board[5]}
+      #{@board.board[6]} #{@board.board[7]} #{@board.board[8]}
+    BOARD
   end
 end
 
